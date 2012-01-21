1b7328b41e187ca5bb66d2f1bfd15b35bba882ec
Gal L <hyprwave@gmail.com>
2012-01-21 18:43:08 +0200

    Beggining work on views.
    A (VERY) basic home view.

--- chores/views.py
+++ chores/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from chores.models import Player, Chore, CompletedChore
+
+def home(request):
+    recently_completed = CompletedChore.objects.all().order_by('-date_completed')[:5]
+    return render_to_response('home.html', {'players' : Player.objects.all(),
+        'recently_completed' : recently_completed})
--- urls.py
+++ urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'chores.views.home', name='home'),
+    url(r'^$', 'chores.views.home', name='home'),
     # url(r'^chores/', include('chores.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
