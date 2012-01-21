d5b26da7494df758bb129cfda33213fe9f9e48c5
Gal L <hyprwave@gmail.com>
2012-01-21 13:13:00 +0200

    Completed Chore model

--- chores/models.py
+++ chores/models.py
@@ -1,5 +1,11 @@
 from django.db import models
+from datetime import datetime
 
 class Chore(models.Model):
     title = models.CharField(max_length=200)
     reward = models.IntegerField()
+
+class CompletedChore(models.Model):
+    chore = models.ForeignKey(Chore)
+    date_completed = Models.DateTimeField(default=datetime.now)
+    player = models.ForeignKey('Player')
