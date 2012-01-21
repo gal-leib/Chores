d8231da14f03b45ef4b336dc0c0fe82fccd50bf0
Gal L <hyprwave@gmail.com>
2012-01-21 21:29:02 +0200

    Base template, using Bootstrap, and tweaking the view a bit

--- chores/views.py
+++ chores/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from chores.models import Player, Chore, CompletedChore
 
 def home(request):
     recently_completed = CompletedChore.objects.all().order_by('-date_completed')[:5]
-    return render_to_response('home.html', {'players' : Player.objects.all(),
+    return render(request, 'home.html', {'players' : Player.objects.all(),
         'recently_completed' : recently_completed})
