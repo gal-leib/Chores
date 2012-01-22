c72b1c24389e7763e38ee31f740d6d9314349578
Gal L <hyprwave@gmail.com>
2012-01-22 13:07:36 +0200

    Progress bar

--- chores/views.py
+++ chores/views.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from django.shortcuts import render, redirect
 from chores.models import Player, Chore, CompletedChore
 from chores.forms import CompleteChoreForm
@@ -7,11 +8,18 @@ from django.core.urlresolvers import reverse
 def home(request):
     recently_completed = CompletedChore.objects.all().order_by('-date_completed')[:5]
     #todo sum all the points so we could use a progress bar
-    return render(request, 'home.html', {'players' : Player.objects.all(),
+    allplayers = Player.objects.all()
+    total_points = sum([p.points for p in allplayers])
+    players=[]
+    for p in allplayers:
+        percent_pts= p.points / total_points * 100
+        players.append((p,percent_pts))
+    return render(request, 'home.html', {'players' : players,
         'recently_completed' : recently_completed, 'form' : CompleteChoreForm()})
 
 class CompleteChoreView(CreateView):
     form_class = CompleteChoreForm
+    template_name = 'complete_chore.html'
 
     def form_valid(self,form):
         self.object = form.save(commit=False)
